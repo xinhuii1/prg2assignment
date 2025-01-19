05e619be3d41c1cd4e9c55014d761ff775b0a2cd
belle5144 <you@example.com>
2025-01-19 18:41:44 +0800

    Completed Feature1

--- Program.cs
+++ Program.cs
@@ -1,3 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using prg2assignment;
 
+//Feature 1
+
+void savingsAccCollection(Dictionary<string, Airline> airlines)
+{
+    string[] csvLines = File.ReadAllLines("airlines.csv");
+    for (int i = 1; i < csvLines.Length; i++)                                // Skip the header row
+    {
+        string[] line = csvLines[i].Split(',');
+        string code = line[0].Trim(); 
+        string name = line[1].Trim(); 
+
+        Airline airline = new Airline(name, code);
+        airlines[code] = airline;
+    }
+}
+
+void LoadBoardingGateData(Dictionary<string, BoardingGate> boardingGates)
+{
+    string[] csvLines = File.ReadAllLines("boardinggates.csv");
+
+    for (int i = 0; i < csvLines.Length; i++) 
+    {
+        string[] fields = csvLines[i].Split(',');
+
+        string gateName = fields[0].Trim();
+        bool supportsDDJB = Convert.ToBoolean(fields[1].Trim());
+        bool supportsCFFT = Convert.ToBoolean(fields[2].Trim());
+        bool supportsLWTT = Convert.ToBoolean(fields[3].Trim());
+
+        BoardingGate gate = new BoardingGate(gateName, supportsDDJB, supportsCFFT, supportsLWTT);
+        boardingGates[gateName] = gate;
+    }
+}
