05acf48cd0180dce48380bc8b8cfaa44f4838014
john_doe <john@example.com>
2025-01-21 20:30:06 +0800

    created NORMFlight class

---
+++ Flight.cs
@@ -0,0 +1,95 @@
+﻿//==========================================================
+// Student Number	: S10269252
+// Student Name	: Han Xin Hui
+// Partner Name	: Belle Chong Qing Xi
+//==========================================================
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prg2assignment
+{
+    abstract class Flight
+    {
+        // attribute
+        private string flightNumber;
+
+        // property
+        public string FlightNumber
+        {
+            get { return flightNumber; }
+            set { flightNumber = value; }
+        }
+
+        private string origin;
+
+        public string Origin
+        {
+            get { return origin; }
+            set { origin = value; }
+        }
+
+        private string destination;
+
+        public string Destination
+        {
+            get { return destination; }
+            set { destination = value; }
+        }
+
+        private DateTime expectedTime;
+
+        public DateTime ExpectedTime
+        {
+            get { return expectedTime; }
+            set { expectedTime = value; }
+        }
+
+        private string status;
+
+        public string Status
+        {
+            get { return status; }
+            set { status = value; }
+        }
+
+        //parameterized constructor
+        public Flight(string flightNumber, string origin, string destination, DateTime expectedTime, string status)
+        {
+            FlightNumber = flightNumber;
+            Origin = origin;
+            Destination = destination;
+            ExpectedTime = expectedTime;
+            Status = status;
+        }
+
+        // method to calculate the total fees
+        public virtual double CalculateFees()
+        {
+            double boardingFee = 300; // base fee for all boarding gates
+
+            // boarding fees based on the origin and destination
+            if (Destination == "Singapore (SIN)")
+            {
+                boardingFee += 500; // arriving flight (SIN)
+            }
+
+            if (Origin == "Singapore (SIN)")
+            {
+                boardingFee += 800; // departing flight (SIN)
+            }
+
+            return boardingFee;
+        }
+
+        // display flight details
+        public virtual override string ToString()
+        {
+            return $"{FlightNumber, -15} {Origin, -25} {Destination, -25} {ExpectedTime:dd/MM/yyyy hh:mm tt}";
+        }
+    }
+}
---
+++ NORMFlight.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prg2assignment
+{
+    // NORMFlight inherits from Flight
+    class NORMFlight : Flight 
+    {
+        // parameterized constructor 
+        public NORMFlight(string flightNumber, string origin, string destination, DateTime expectedTime, string status) 
+            : base(flightNumber, origin, destination, expectedTime, status)
+        {
+
+        }
+
+        public override double CalculateFees()
+        {
+            return base.CalculateFees();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString(); // same format as base class
+        }
+    }
+}
